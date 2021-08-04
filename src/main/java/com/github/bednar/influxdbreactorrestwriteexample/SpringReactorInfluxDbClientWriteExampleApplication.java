fd87be2c206f0db321f3e4a830b8cddff8c94bb9
Jakub Bednar <jakub.bednar@gmail.com>
2021-08-04 12:28:43 +0200

    feat: use autoconfigure to bootstrap client

--- src/main/java/com/github/bednar/influxdbreactorrestwriteexample/SpringReactorInfluxDbClientWriteExampleApplication.java
+++ src/main/java/com/github/bednar/influxdbreactorrestwriteexample/SpringReactorInfluxDbClientWriteExampleApplication.java
@@ -1,24 +1,12 @@
 package com.github.bednar.influxdbreactorrestwriteexample;
 
-import com.influxdb.LogLevel;
-import com.influxdb.client.InfluxDBClient;
-import com.influxdb.client.InfluxDBClientFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringReactorInfluxDbClientWriteExampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringReactorInfluxDbClientWriteExampleApplication.class, args);
-    }
-
-    @Bean
-    public InfluxDBClient influxDBClient() {
-        String url = "http://localhost:8086";
-        char[] token = "my-token".toCharArray();
-
-        return InfluxDBClientFactory.create(url, token).setLogLevel(LogLevel.BODY);
     }
 }
